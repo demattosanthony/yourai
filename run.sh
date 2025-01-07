8fca743f590952d17fb0f0caa4b4322f395a4d52
Anthony DeMattos <demattosanthony@gmail.com>
2025-01-06 20:21:13 -0500

    added run bash script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run the Express API
+cd api/
+bun run dev &
+SERVER_PID=$!
+echo "Express api started with PID $SERVER_PID"
+
+# Run the Next.js app
+cd ../frontend/
+bun run dev &
+NEXTJS_APP_PID=$!
+echo "Next.js app started with PID $NEXTJS_APP_PID"
+
+# Wait for both processes to complete
+wait $SERVER_PID $NEXTJS_APP_PID
+
+# If either process exits, the script will clean up
+trap "echo 'Stopping both processes'; kill $SERVER_PID $NEXTJS_APP_PID" EXIT
