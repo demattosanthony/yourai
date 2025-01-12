a30cdff446390f40eccbf66e592825cd00d690ac
Anthony DeMattos <demattosanthony@gmail.com>
2025-01-12 14:16:20 -0500

    removed maskable

--- frontend/app/manifest.ts
+++ frontend/app/manifest.ts
@@ -15,13 +15,11 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/YO_192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
       },
       {
         src: "/YO_512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
       },
     ],
   };
