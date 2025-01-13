8e2d99f92cca726ed4a2772a2cd62ab74d3b9496
Anthony DeMattos <demattosanthony@gmail.com>
2025-01-12 19:49:08 -0500

    removed s3 region

--- api/app/config/s3.ts
+++ api/app/config/s3.ts
@@ -5,7 +5,6 @@ const s3 = new S3Client({
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   endpoint: process.env.S3_ENDPOINT_URL,
   bucket: process.env.S3_BUCKET_NAME,
-  region: process.env.S3_REGION,
 });
 
 export default s3;
