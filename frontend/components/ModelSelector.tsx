15cc2ef2769190eb28d3659d6282283921b2e5c3
Anthony DeMattos <demattosanthony@gmail.com>
2025-02-05 12:52:33 -0500

    fixed auto select icon placement

--- frontend/components/ModelSelector.tsx
+++ frontend/components/ModelSelector.tsx
@@ -96,7 +96,7 @@ const ModelSelector: React.FC = () => {
                     }}
                   >
                     <div className="flex items-center">
-                      <WandSparkles className="w-4 h-4 mr-2" />
+                      <WandSparkles className="w-5 h-5 mr-2 p-[2px]" />
                       <span>Auto</span>
                     </div>
                     <Check
